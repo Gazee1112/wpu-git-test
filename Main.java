d0dbd8b5d9e6fbaac10fee0fcbffd42c88b10b41
Gazee1112 <muksinsaja12@gmail.com>
2022-03-02 13:53:43 +0700

    MEnambhakan paragrap

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Mencoba git remote");
+        System.out.println("Menambahkanfile baru");
+        System.out.println("Menambahkan file baru");
     }
 }
